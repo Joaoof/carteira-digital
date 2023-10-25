8be503d35a673f08352a7efa31e0da7d34eb55b4
Joaoof <joaodeus400@gmail.com>
2023-10-25 10:05:34 -0300

    introdução a rotas nas arquiteturas em camadas

--- app/Controllers/Http/Auth/AuthController.ts
+++ app/Controllers/Http/Auth/AuthController.ts
@@ -2,9 +2,13 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import AuthService from 'App/Service/AuthService'
 
 export default class AuthController {
-    public async signup({request, response}: HttpContextContract) {
+    public async signup({ request, response }: HttpContextContract) {
         const body = request.body()
+        console.log(body);
 
-        return  AuthService.signup(body)
+        const resService = AuthService.signup(body)
+        response.send(resService)
     }
 }
+
+
