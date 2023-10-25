399162212ec7dec3799a80f9e0a17d7c9a9fc762
Joaoof <joaodeus400@gmail.com>
2023-10-25 15:27:28 -0300

    trativa de error controler

--- app/Controllers/Http/Auth/AuthController.ts
+++ app/Controllers/Http/Auth/AuthController.ts
@@ -3,11 +3,16 @@ import AuthService from 'App/Service/AuthService'
 
 export default class AuthController {
     public async signup({ request, response }: HttpContextContract) {
-        const body = request.body()
-            const resService = await AuthService.signup(body)
-            response.send(resService)
-        }
-    }
-    
 
+        try {
+            const body = request.body()
+            const resService = await AuthService.signup(body)
+            response.status(201).send(resService)
+        }
+        catch (error) {
+            response.status(409).json({ message: error.message })
+        }
+
+    }
+}
 
